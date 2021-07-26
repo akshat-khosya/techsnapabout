e6a6e3de2ee81c76b5fa4077a97e2acf6a16feea
Akshat Khosya <akshatdps12@gmail.com>
2021-07-26 15:45:40 +0530

    third commit

--- contact.js
+++ contact.js
@@ -1,5 +1,7 @@
 const fab =document.querySelector(".FAB");
 const icons= document.querySelector(".icons");
+const texts=document.querySelector(".contactus__text");
 fab.addEventListener("click", ()=>{
     icons.classList.toggle("open");
+    texts.classList.toggle("contactus__text-open");
 })
